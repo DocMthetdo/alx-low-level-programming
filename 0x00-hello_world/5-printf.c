83a726f53ec66c46c568e5d1fe42d97c9d0857f9
DocMthetdo <doctormthethwa@gmail.com>
2023-07-13 07:39:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses prinf
+ * Return 0 (Successful)
+ */
+int main(void)
+{printf("with proper grammer, but the outcome is a piece of art, \n");
+return (0);
+}
+
