78491dba7ce7c050b906e5b5e8fde25d2b4ec505
DocMthetdo <doctormthethwa@gmail.com>
2023-07-13 06:40:05 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Programming print aline with a put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
